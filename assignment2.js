3870ff7bfd53c7b0b24934f27ec3d53cffdf7609
Ronald Atutubo <ronaldatutubo@gmail.com>
2024-03-21 16:20:12 -0600

    Initial Push

---
+++ assignment2.js
@@ -0,0 +1,14 @@
+function handleButtonClick(buttonText) {
+    console.log(buttonText + ' button clicked!');
+}
+
+function toggleAccordion(sectionId) {
+    const section = document.getElementById(sectionId);
+    if (section.style.display === 'block') {
+        section.style.display = 'none';
+    } else {
+        section.style.display = 'block';
+    }
+}
+
+document.getElementById("currentYear").innerHTML = new Date().getFullYear();
